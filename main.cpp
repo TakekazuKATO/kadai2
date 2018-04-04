4e6514f8b9d96fef9b8c29f52fcb0fa1e1350f3c
Takekazu KATO <takekazu.kato@gmail.com>
2018-04-04 10:07:41 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int fibonacchi(int x){
+    int y;
+    /*
+     * ここにx番目のフィボナッチ数を計算するプログラムを書く
+     */
+    return y;
+}
+int main() {
+    printf("f(%d)=%d",10,fibonacchi(10));
+    return 0;
+}
